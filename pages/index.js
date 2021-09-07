910b204447b4e2462fd99d9fee8c277dc5de46f3
Jo Sprague <git@iamlocaljo.com>
2021-09-07 17:45:50 +0200

    Try large images

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,8 @@ import Image from 'next/image';
 import { ReactCompareSlider } from 'react-compare-slider';
 
 function Home() {
-    console.log('Render');
-
-    const urlOne = 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Netlify_logo.svg/147px-Netlify_logo.svg.png';
-    const urlTwo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Netlify_logo.svg/320px-Netlify_logo.svg.png';
+    const urlOne = 'https://assets.climatecentral.org/mc2-ge-still/GBR__0__Glasgow__SEC_Centre__L13__present.jpg';
+    const urlTwo = 'https://assets.climatecentral.org/mc2-ge-still/GBR__0__Glasgow__SEC_Centre__L13__4p0C.jpg';
 
     const [expectedAssets, setExpectedAssets] = useState([]);
     const [assetsLoaded, setAssetsLoaded] = useState([]);
@@ -18,39 +16,44 @@ function Home() {
     }, []);
 
     const isDoneLoading = assetsLoaded.length >= expectedAssets.length;
+
     return (
         <>
             <p>Is loading done? {isDoneLoading ? 'Yes' : 'No'}</p>
             <ReactCompareSlider
                 itemOne={
-                    <Image
-                        src={urlOne}
-                        width='147'
-                        height='40'
-                        alt='Example image one'
-                        onLoadingComplete={(width, height) => {
-                            if (!assetsLoaded.includes(urlOne)) {
-                                console.log('Loaded one', width, height);
-                                const allAssets = [...assetsLoaded, urlOne];
-                                setAssetsLoaded([...new Set(allAssets)]);
-                            }
-                        }}
-                    />
+                    <div style={{ position: 'relative', height: '100vh' }}>
+                        <Image
+                            src={urlOne}
+                            layout='fill'
+                            objectFit='cover'
+                            alt='Example image one'
+                            onLoadingComplete={(width, height) => {
+                                if (!assetsLoaded.includes(urlOne)) {
+                                    console.log('Loaded one', width, height);
+                                    const allAssets = [...assetsLoaded, urlOne];
+                                    setAssetsLoaded([...new Set(allAssets)]);
+                                }
+                            }}
+                        />
+                    </div>
                 }
                 itemTwo={
-                    <Image
-                        src={urlTwo}
-                        width='147'
-                        height='40'
-                        alt='Example image two'
-                        onLoadingComplete={(width, height) => {
-                            if (!assetsLoaded.includes(urlTwo)) {
-                                console.log('Loaded two', width, height);
-                                const allAssets = [...assetsLoaded, urlTwo];
-                                setAssetsLoaded([...new Set(allAssets)]);
-                            }
-                        }}
-                    />
+                    <div style={{ position: 'relative', height: '100vh' }}>
+                        <Image
+                            src={urlTwo}
+                            layout='fill'
+                            objectFit='cover'
+                            alt='Example image two'
+                            onLoadingComplete={(width, height) => {
+                                if (!assetsLoaded.includes(urlTwo)) {
+                                    console.log('Loaded two', width, height);
+                                    const allAssets = [...assetsLoaded, urlTwo];
+                                    setAssetsLoaded([...new Set(allAssets)]);
+                                }
+                            }}
+                        />
+                    </div>
                 }
             />
         </>
