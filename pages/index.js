7522771dac243e5ed5aea4f6974f8962c3fe0e15
Jo Sprague <git@iamlocaljo.com>
2021-09-07 17:01:52 +0200

    Add ReactCompareSlider

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,37 @@
 import Image from 'next/image';
+import { ReactCompareSlider } from 'react-compare-slider';
 
 function Home() {
-  console.log('Render');
-  return (
-    <>
-      <p>onLoadingComplete test</p>
-      <Image
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Netlify_logo.svg/147px-Netlify_logo.svg.png"
-        width="147"
-        height="40"
-        alt="Example image"
-        onLoadingComplete={(width, height)=>{
-          console.log('Loaded', width, height);
-        }}
-      />
-    </>
-  )
+    console.log('Render');
+    return (
+        <>
+            <p>onLoadingComplete test</p>
+            <ReactCompareSlider
+                itemOne={
+                    <Image
+                        src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Netlify_logo.svg/147px-Netlify_logo.svg.png'
+                        width='147'
+                        height='40'
+                        alt='Example image one'
+                        onLoadingComplete={(width, height) => {
+                            console.log('Loaded one', width, height);
+                        }}
+                    />
+                }
+                itemTwo={
+                    <Image
+                        src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Netlify_logo.svg/147px-Netlify_logo.svg.png'
+                        width='147'
+                        height='40'
+                        alt='Example image two'
+                        onLoadingComplete={(width, height) => {
+                            console.log('Loaded two', width, height);
+                        }}
+                    />
+                }
+            />
+        </>
+    );
 }
 
 export default Home
